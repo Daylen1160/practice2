8b02aef32e3e01af9b5e7c797c2cfdb510aabbe9
Daylen1160 <daylen924@gmail.com>
2025-10-20 15:08:40 -0700

    Finished practice2

--- src/main/java/cpen221practice/Resistor.java
+++ src/main/java/cpen221practice/Resistor.java
@@ -2,10 +2,22 @@ package cpen221practice;
 
 class Resistor {
     int colorCode(String color) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return switch (color) {
+            case "black" -> 0;
+            case "brown" -> 1;
+            case "red" -> 2;
+            case "orange" -> 3;
+            case "yellow" -> 4;
+            case "green" -> 5;
+            case "blue" -> 6;
+            case "violet" -> 7;
+            case "grey" -> 8;
+            case "white" -> 9;
+            default -> -1;
+        };
     }
 
     String[] colors() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return new String[] {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
     }
 }
